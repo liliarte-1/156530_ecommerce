26572fa230ebb93d0243a8f1b7938616456bd659
lili liarte <alu.156530@usj.es>
2025-12-16 18:24:44 +0100

    va_porfa

--- react-router/backend/app/db.py
+++ react-router/backend/app/db.py
@@ -10,8 +10,8 @@ database_url = os.environ.get("DATABASE_URL")
 # database_url = "postgresql://postgres.pngzytvvzjcvxzmeoaih:estanoesbroki"34@aws-1-eu-central-1.pooler.supabase.com:5432/postgres"
 
 connect_args = {"check_same_thread": False} 
-engine = create_engine(database_url, connect_args=connect_args)
-
+# engine = create_engine(database_url, connect_args=connect_args)
+engine = create_engine(database_url, echo=True)
 
 def create_db_and_tables():
     # Import models so SQLModel metadata includes them when creating tables
